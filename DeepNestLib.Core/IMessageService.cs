717f44b0f5b01b1bd1b51c8c9b22f9ab2242be76
Chase Richards <crichards@blairimage.com>
2024-03-26 14:38:10 -0400

    Added functions to IMessageService interface.

--- DeepNestLib.Core/IMessageService.cs
+++ DeepNestLib.Core/IMessageService.cs
@@ -10,6 +10,22 @@
 
     void DisplayMessageBox(string text, string caption, MessageBoxIcon icon);
 
+    void StartRecord();
+
+    void StopRecord();
+
+    /// <summary>
+    /// Print the record to the console or other output. Just pass the action from the message service.
+    /// </summary>
+    /// <param name="action"></param>
+    void PrintRecord(Action<string> action);
+
+    void SaveRecord(string path);
+
+    void WriteToDebugConsole(string message);
+
+    void WriteToDebugConsole(Exception ex);
+
     MessageBoxResult DisplayOkCancel(string text, string caption, MessageBoxIcon icon);
 
     MessageBoxResult DisplayYesNoCancel(string text, string caption, MessageBoxIcon icon);
