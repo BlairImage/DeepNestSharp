2c59cda504f360aa1c77169c023567821e6a694a
Chase Richards <crichards@blairimage.com>
2024-06-14 15:51:07 -0400

    Made Sheets notifying

--- DeepNestLib.Core/IMaterial.cs
+++ DeepNestLib.Core/IMaterial.cs
@@ -1,6 +1,7 @@
 ﻿namespace DeepNestLib
 {
   using System.Collections.Generic;
+  using System.Collections.ObjectModel;
   using NestProject;
 
   public interface IMaterial
@@ -13,14 +14,14 @@
 
     public byte[] ImageData { get; set; }
 
-    public List<ISheetLoadInfo> StockSize { get; set; }
+    public ObservableCollection<ISheetLoadInfo> StockSize { get; set; }
 
     public string DisplayName => $"{Name}: ${Cost}";
 
-    public void AddStockSize(int width, int height, int quantity);
+    public void AddStockSize(int width, int height, int quantity = 0);
 
-    public void AddStockSize(SheetLoadInfo sheetLoadInfo);
+    public void AddStockSize(ISheetLoadInfo sheetLoadInfo);
 
-    public void AddStockSizeRange(IEnumerable<SheetLoadInfo> sheetLoadInfos);
+    public void AddStockSizeRange(IEnumerable<ISheetLoadInfo> sheetLoadInfos);
   }
 }
--- DeepNestLib.Core/NestProject/SheetLoadInfo.cs
+++ DeepNestLib.Core/NestProject/SheetLoadInfo.cs
@@ -1,10 +1,13 @@
 ﻿namespace DeepNestLib.NestProject
 {
+  using System.Collections.Generic;
+  using System.ComponentModel;
+  using System.Runtime.CompilerServices;
   using System.Text.Json;
   using System.Text.Json.Serialization;
   using IO;
 
-  public class SheetLoadInfo : Saveable, ISheetLoadInfo
+  public class SheetLoadInfo : Saveable, ISheetLoadInfo, INotifyPropertyChanged
   {
     [JsonConstructor]
     public SheetLoadInfo(int width, int height, int quantity)
@@ -22,19 +25,56 @@
       Material = material;
     }
 
-    public virtual int Width { get; set; }
+    private int m_Width;
 
-    public virtual int Height { get; set; }
+    public virtual int Width
+    {
+      get => m_Width;
+      set => SetField(ref m_Width, value);
+    }
 
-    public virtual int Quantity { get; set; }
+    private int m_Height;
+    public virtual int Height
+    {
+      get => m_Height;
+      set => SetField(ref m_Height, value);
+    }
 
-    public virtual IMaterial Material { get; set; }
+    private int m_Quantity;
+    public virtual int Quantity
+    {
+      get => m_Quantity;
+      set => SetField(ref m_Quantity, value);
+    }
+
+    private IMaterial m_Material;
+    public virtual IMaterial Material
+    {
+      get => m_Material;
+      set => SetField(ref m_Material, value);
+    }
 
     public override string ToJson(bool writeIndented = false)
     {
       JsonSerializerOptions options = new JsonSerializerOptions();
       options.WriteIndented = writeIndented;
       return JsonSerializer.Serialize(this, options);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+      if (EqualityComparer<T>.Default.Equals(field, value))
+        return false;
+      field = value;
+      OnPropertyChanged(propertyName);
+      return true;
     }
   }
 }
